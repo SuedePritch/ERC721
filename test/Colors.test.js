e898672d84ad94b367311b83862373717e79e4ab
James Pritchard <james.edward.pritchard@gmail.com>
2022-01-31 13:25:53 -0500

    setup

---
+++ test/Colors.test.js
@@ -0,0 +1,10 @@
+const Colors = artifacts.require("Colors")
+require('chai')
+
+contract('Colors', (accounts)=>{
+    describe('contract deployment', async()=>{
+        it('color contract deploys successfully', async()=>{
+            
+        })
+})
+})

a32dc587c29e92efc4ae1cff023a8847fde920f7
James Pritchard <james.edward.pritchard@gmail.com>
2022-02-01 17:30:59 -0500

    fixed totalSupply bug

--- contracts/Colors.sol
+++ contracts/Colors.sol
@@ -1,8 +1,39 @@
 // SPDX-License-Identifier: MIT
-pragma solidity >=0.4.22 <0.9.0;
+pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-contract Colors is ERC721{
-    constructor() ERC721("Colors", "COLOR") {
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+contract Colors is ERC721, ERC721Enumerable{
+    string[] public colors;
+    mapping(string =>bool) _colorExists;
+    constructor() ERC721("Colors", "COLOR"){
     }
+
+    function _beforeTokenTransfer(address from, address to, uint256 tokenId)
+    internal
+    override(ERC721, ERC721Enumerable)
+    {
+        super._beforeTokenTransfer(from, to, tokenId);
+    }
+
+    function supportsInterface(bytes4 interfaceId)
+    public
+    view
+    override(ERC721, ERC721Enumerable)
+    returns (bool)
+    {
+        return super.supportsInterface(interfaceId);
+    }
+
+
+
+    function mint(string memory _color) public {
+    require(!_colorExists[_color]);
+
+    colors.push(_color);
+    uint _id = colors.length;
+    _mint(msg.sender, _id);
+    _colorExists[_color] = true;
+    }
+
 }
--- test/Colors.test.js
+++ test/Colors.test.js
@@ -1,10 +1,80 @@
+
+var chai = require("chai");
+var chaiAsPromised = require("chai-as-promised");
+
+chai.use(chaiAsPromised);
+
+var assert = chai.assert;
+chai.should();
+
+
 const Colors = artifacts.require("Colors")
 require('chai')
 
 contract('Colors', (accounts)=>{
+    let contract
+    before(async ()=>{
+        contract = await Colors.deployed()
+    })
     describe('contract deployment', async()=>{
         it('color contract deploys successfully', async()=>{
-            
+            const address = contract.address
+            assert.notEqual(address, '')
+            assert.notEqual(address, 0x0)
+            assert.notEqual(address, null)
+            assert.notEqual(address, undefined)
         })
+        it('has a name', async () =>{
+            name = await contract.name()
+            assert.equal(name, 'Colors')
+        })
+        it('has a symbol', async () =>{
+            symbol = await contract.symbol()
+            assert.equal(symbol, 'COLOR')
+        })
+        
+})
+describe('minting', async()=>{
+    it('creates new token', async()=>{
+        const result = await contract.mint('#333333')
+        //plus one just so it starts at 1 instead of array[0]
+        const totalSupply = await contract.colors.length +1;
+        
+        //SUCCESS
+        // console.log(totalSupply)
+        assert.equal(totalSupply, 1)
+        // console.log(result);
+        const event = result.logs[0].args
+        // console.log(event);
+        assert.equal(event.tokenId.toNumber(),1, 'id correct')
+        assert.equal(event.from, '0x0000000000000000000000000000000000000000', 'from')
+        assert.equal(event.to, accounts[0], 'to')
+
+        //FAILURE
+        await contract.mint('#333333').should.be.rejected;
+        // await contract.mint('#333332').should.be.rejected; //it is accepted because it is a new color
+    })
+    
+})
+describe('indexing', async () => {
+    it('lists colors', async () => {
+      // Mint 3 more tokens
+        await contract.mint('#5386E4')
+        await contract.mint('#FFFFFF')
+        await contract.mint('#000000')
+        const totalSupply = await contract.totalSupply()
+
+        let color
+        let result = []
+
+        for (var i = 1; i <= totalSupply; i++) {
+        color = await contract.colors(i - 1)
+        result.push(color)
+        }
+
+        let expected = ['#333333', '#5386E4', '#FFFFFF', '#000000']
+        assert.equal(result.join(','), expected.join(','))
+    })
+
 })
 })
